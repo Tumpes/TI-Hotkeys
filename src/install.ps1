3ad299dab6a77728d9e4aaa928e15222a93fd093
Tumpes <69531945+Tumpes@users.noreply.github.com>
2024-05-08 11:35:38 +0300

    fixed script installation

--- src/install.ps1
+++ src/install.ps1
@@ -8,6 +8,9 @@ if (Test-Path "$env:APPDATA\TI-Hotkeys\TI-Hotkeys.ahk") {
  # update the script
     $wc.DownloadFile("https://raw.githubusercontent.com/Tumpes/TI-Hotkeys/master/src/TI-Hotkeys.ahk", "$env:APPDATA\TI-Hotkeys\TI-Hotkeys.ahk")
 }
+else {
+    $wc.DownloadFile("https://raw.githubusercontent.com/Tumpes/TI-Hotkeys/master/src/TI-Hotkeys.ahk", "$env:APPDATA\TI-Hotkeys\TI-Hotkeys.ahk")
+}
 
 if (!Test-Path "$env:APPDATA\TI-Hotkeys\AutoHotkey64.exe") {
     $wc.DownloadFile("https://github.com/Tumpes/TI-Hotkeys/raw/master/src/AutoHotkey64.exe", "$env:APPDATA\TI-Hotkeys\AutoHotkey64.exe")
